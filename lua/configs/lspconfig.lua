cb8d64f440851179e9f0602ed717da2107b3a017
Landon <landonp95@gmail.com>
2026-05-10 19:59:37 -0400

    zls & rose-pine theme

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "gopls", "rust_analyzer", "c3-lsp", "mesonlsp", "markdown_oxide", "svelte-language-server", "hls", "basedpyright", "ruff", "ltex_plus" }
+local servers = { "clangd", "gopls", "rust_analyzer", "c3-lsp", "mesonlsp", "markdown_oxide", "svelte-language-server", "hls", "basedpyright", "ruff", "ltex_plus", "typescript-language-server", "zls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
